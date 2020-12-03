99968b426db5f83c8cac4b127d72083368ea93a0
Marvin Ma <marvin.ma@tapad.com>
2020-12-02 19:37:17 -0500

    Day02: Using scala 2.13 string extractor pattern.

--- src/main/scala/aoc/Day02.scala
+++ src/main/scala/aoc/Day02.scala
@@ -1,25 +1,22 @@
 package aoc
 
-import scala.util.matching.Regex
-
 object Day02 extends App {
 
-  def solution1(pwConfigs: Seq[String], regex: Regex): Int = {
-    pwConfigs.count { case regex(min, max, letter, pw) =>
+  def solution1(pwConfigs: Seq[String]): Int = {
+    pwConfigs.count { case s"$min-$max $letter: $pw" =>
       val numLetters = pw.count(_ == letter(0))
       min.toInt <= numLetters && max.toInt >= numLetters
     }
   }
 
-  def solution2(pwConfigs: Seq[String], regex: Regex): Int = {
-    pwConfigs.count { case regex(pos1, pos2, letter, pw) =>
+  def solution2(pwConfigs: Seq[String]): Int = {
+    pwConfigs.count { case s"$pos1-$pos2 $letter: $pw" =>
       (pw(pos1.toInt-1) == letter(0)) != (pw(pos2.toInt-1) == letter(0))
     }
   }
 
   val pwConfigs = io.Source.fromResource("day02.txt").getLines.toSeq
-  val regex = """(\d+)-(\d+) (\w): (\w+)""".r
 
-  println(solution1(pwConfigs, regex))
-  println(solution2(pwConfigs, regex))
+  println(solution1(pwConfigs))
+  println(solution2(pwConfigs))
 }
