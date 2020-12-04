66e3c7e6fc73bad3f79ad84c6dec977edf416177
Marvin Ma <marvin.ma@tapad.com>
2020-12-04 12:15:42 -0500

    Day04: Cleaned up validations.

--- src/main/scala/aoc/Day04.scala
+++ src/main/scala/aoc/Day04.scala
@@ -17,46 +17,42 @@ object Day04 extends App {
 
   def solution1(passports: Seq[String]): Int = {
     val validProperties = Seq("byr:","iyr:","eyr:","hgt:","hcl:","ecl:","pid:")
-    passports
-      .count { config =>
-        validProperties.forall(config.contains)
-      }
+    passports.count { config =>
+      validProperties.forall(config.contains)
+    }
   }
 
   def solution2(passports: Seq[String]): Int = {
+    def validateYear(m: Match, min: Int, max: Int): Boolean = {
+      val year = m.group(1).toInt
+      year >= min && year <= max
+    }
+
+    def validateHeight(m: Match): Boolean = {
+      val height = m.group(1).toInt
+      m.group(2) match {
+        case "cm" => height >= 150 && height <= 193
+        case _    => height >= 59 && height <= 76
+      }
+    }
+
     val validProperties: Seq[(Regex, Match => Boolean)] = Seq(
-      "byr:(\\d+)\\s".r -> { m =>
-        val year = m.group(1).toInt
-        year >= 1920 && year <= 2002
-      },
-      "iyr:(\\d+)\\s".r -> { m =>
-        val year = m.group(1).toInt
-        year >= 2010 && year <= 2020
-      },
-      "eyr:(\\d+)\\s".r -> { m =>
-        val year = m.group(1).toInt
-        year >= 2020 && year <= 2030
-      },
-      "hgt:(\\d+)(cm|in)\\s".r -> { m =>
-        val height = m.group(1).toInt
-        m.group(2) match {
-          case "cm" => height >= 150 && height <= 193
-          case _    => height >= 59 && height <= 76
-        }
-      },
-      "hcl:(#[0-9a-f]{6})\\s".r -> {_ => true},
+      "byr:(\\d+)\\s".r                        -> {validateYear(_, 1920, 2002)},
+      "iyr:(\\d+)\\s".r                        -> {validateYear(_, 2010, 2020)},
+      "eyr:(\\d+)\\s".r                        -> {validateYear(_, 2020, 2030)},
+      "hgt:(\\d+)(cm|in)\\s".r                 -> validateHeight,
+      "hcl:(#[0-9a-f]{6})\\s".r                -> {_ => true},
       "ecl:(amb|blu|brn|gry|grn|hzl|oth)\\s".r -> {_ => true},
-      "pid:(\\d{9})\\s".r -> {_ => true}
+      "pid:(\\d{9})\\s".r                      -> {_ => true}
     )
 
-    passports
-      .count { config =>
-        validProperties.forall { case (regex, validationFn) =>
-          regex
-            .findFirstMatchIn(config)
-            .exists(validationFn)
-        }
+    passports.count { config =>
+      validProperties.forall { case (regex, validationFn) =>
+        regex
+          .findFirstMatchIn(config)
+          .exists(validationFn)
       }
+    }
   }
 
   val passports = consumeText(io.Source.fromResource("day04.txt").getLines.toSeq)
