b1c59dd219272628d36c234b73b769720a4eb43e
Marvin Ma <marvin.ma@tapad.com>
2021-12-08 01:43:42 -0500

    Housekeeping.

---
+++ src/main/scala/TemplateApp.scala
@@ -0,0 +1,13 @@
+object TemplateApp extends App {
+
+  def solution1(lines: List[String]) = {
+  }
+
+//  def solution2(lines: List[String]) = {
+//  }
+
+  val lines = io.Source.fromResource("2021/day01.txt").getLines.toList
+
+  println(solution1(lines))
+//  println(solution2(lines))
+}
