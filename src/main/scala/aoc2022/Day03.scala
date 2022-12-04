db4195953c592563d12ffa20823d06623325ad63
Marvin Ma <marvin.ma@tapad.com>
2022-12-03 23:58:51 -0500

    Day03: Can't forget .grouped().

---
+++ src/main/scala/aoc2022/Day03.scala
@@ -0,0 +1,32 @@
+package aoc2022
+
+object Day03 extends App {
+  def toPriority(c: Char): Int = {
+    if(c.isLower) c.toInt - 96
+    else          c.toInt - 38
+  }
+
+  def solution1(rucksacks: List[String]): Int = {
+    rucksacks.map { sack =>
+      val (first, second) = sack.splitAt(sack.length/2)
+
+      toPriority(first.intersect(second).head)
+    }
+    .sum
+  }
+
+  def solution2(rucksacks: List[String]): Int = {
+    rucksacks
+      .grouped(3)
+      .map {
+        case first :: second :: third :: Nil => first.intersect(second).intersect(third).head
+      }
+      .map(toPriority)
+      .sum
+  }
+
+  val nums = io.Source.fromResource("2022/day03.txt").getLines.toList
+
+  println(solution1(nums))
+  println(solution2(nums))
+}
